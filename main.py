8b881d2a05edbc5c93645c16d94a2fb265134893
Aymen-Kassab <aymankassab123@gmail.com>
2025-09-13 17:10:12 +0100

    feat: setting up the application

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello to calculator program!")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+import math
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon("./logo.png"))
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
